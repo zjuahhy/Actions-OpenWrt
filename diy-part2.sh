7df5204225c454360d3b78df173827fe7fd4169f
zjuahhy <136286080+zjuahhy@users.noreply.github.com>
2026-01-03 10:15:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -20,6 +20,25 @@
 #sed -i 's/ABABAABB/LUNZN-S20P/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-ccc-ddd.dts
 #rm -rf target/linux/mediatek/patches-5.4/9921-support-SX-7981R128-for-mtk-sdk-v7.6.6.1.patch
 
+# Add packages
+#git clone -b js https://github.com/gngpp/luci-theme-design package/luci-theme-design
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
+#git clone https://github.com/sirpdboy/netspeedtest package/homebox
+#git clone https://github.com/destan19/OpenAppFilter package/openappfilter
+#rm -rf feeds/packages/net/open-app-filter
+
 # Modify default WiFi SSID
 #sed -i 's/ImmortalWrt-2.4G/CLX2.4G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 #sed -i 's/ImmortalWrt-5G/CLX5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+# Add OpenClash DEV/TUN core
+cd ./feeds/luci/applications/luci-app-openclash/root/etc/openclash/
+mkdir ./core && cd ./core
+#curl -sfL -o ./dev.tar.gz https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-arm64.tar.gz
+#tar -zxf ./dev.tar.gz
+#curl -sfL -o ./clash_tun.gz https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz
+#gzip -d clash_tun.gz
+curl -sfL -o ./meta.tar.gz https://github.com/zjuahhy/Actions-OpenWrt/raw/main/hanwckf/clash-linux-arm64.tar.gz
+tar -zxf ./meta.tar.gz && mv -f clash clash_meta
+chmod +x ./clash* ; rm -rf ./*.gz
